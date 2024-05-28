e343ba93c920d59eb0dd7385e2c94944fb306af5
Natalia Hristova <128708780+SergioFerreras@users.noreply.github.com>
2024-05-28 16:23:43 +0200

    Actualizacion 4.0

---
+++ src/main/java/org/example/gympro/Controller/ShoulderMachinesController.java
@@ -0,0 +1,116 @@
+package org.example.gympro.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import org.example.gympro.Clases.User;
+
+public class ShoulderMachinesController {
+    User user;
+    @FXML
+    private Button backButton;
+    @FXML
+    private Button crossButton;
+    @FXML
+    private Button peckdeckbutton1;
+    @FXML
+    private Button peckdeckbutton2;
+    @FXML
+    private Button peckdeckbutton3;
+
+    @FXML
+    private void cerrarVentana(ActionEvent event) {
+        Stage stage = (Stage) crossButton.getScene().getWindow();
+        stage.close();
+    }
+    @FXML
+    private void irAtras(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/MyGym.fxml"));
+            Parent otraPaginaParent = loader.load();
+            Scene otraPaginaScene = new Scene(otraPaginaParent);
+
+            MyGymController controller=loader.getController();
+            controller.setUser(getUser());
+
+            Stage escenarioActual = (Stage) backButton.getScene().getWindow();
+            escenarioActual.setScene(otraPaginaScene);
+        } catch (Exception e) {
+            System.err.println("Error: "+e.getMessage());
+            Stage stage = (Stage) crossButton.getScene().getWindow();
+            stage.close();
+        }
+    }
+
+    @FXML
+    private void irPeckDeckExplication(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/MachineInfo.fxml"));
+            Parent otraPaginaParent = loader.load();
+            Scene otraPaginaScene = new Scene(otraPaginaParent);
+
+            MachineInfoController controller=loader.getController();
+            controller.setUser(getUser());
+            controller.setNumero(13);
+
+            Stage escenarioActual = (Stage) backButton.getScene().getWindow();
+            escenarioActual.setScene(otraPaginaScene);
+        } catch (Exception e) {
+            System.err.println("Error: "+e.getMessage());
+            Stage stage = (Stage) crossButton.getScene().getWindow();
+            stage.close();
+        }
+    }
+
+    @FXML
+    private void irPeckDeckExplication2(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/MachineInfo.fxml"));
+            Parent otraPaginaParent = loader.load();
+            Scene otraPaginaScene = new Scene(otraPaginaParent);
+
+            MachineInfoController controller=loader.getController();
+            controller.setUser(getUser());
+            controller.setNumero(14);
+
+            Stage escenarioActual = (Stage) backButton.getScene().getWindow();
+            escenarioActual.setScene(otraPaginaScene);
+        } catch (Exception e) {
+            System.err.println("Error: "+e.getMessage());
+            Stage stage = (Stage) crossButton.getScene().getWindow();
+            stage.close();
+        }
+    }
+
+    @FXML
+    private void irPeckDeckExplication3(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/MachineInfo.fxml"));
+            Parent otraPaginaParent = loader.load();
+            Scene otraPaginaScene = new Scene(otraPaginaParent);
+
+            MachineInfoController controller=loader.getController();
+            controller.setUser(getUser());
+            controller.setNumero(15);
+
+            Stage escenarioActual = (Stage) backButton.getScene().getWindow();
+            escenarioActual.setScene(otraPaginaScene);
+        } catch (Exception e) {
+            System.err.println("Error: "+e.getMessage());
+            Stage stage = (Stage) crossButton.getScene().getWindow();
+            stage.close();
+        }
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
